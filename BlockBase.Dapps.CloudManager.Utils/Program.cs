8f1f64f60a555e6dbb709ede23a32e34f9f01276
josecoelh <jose.coelho2@gmail.com>
2021-01-28 12:30:38 +0000

    Added project utils

---
+++ BlockBase.Dapps.CloudManager.Utils/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BlockBase.Dapps.CloudManager.Utils
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
