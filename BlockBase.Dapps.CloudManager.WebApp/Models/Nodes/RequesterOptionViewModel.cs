4ab23aaa1d8319470b5c680930777774eee80192
josecoelh <jose.coelho2@gmail.com>
2021-02-03 17:36:11 +0000

    RequesterConfigurations View Done, missing some back-end implementation still

--- BlockBase.Dapps.CloudManager.WebApp/Models/Nodes/RequesterOptionViewModel.cs
+++ BlockBase.Dapps.CloudManager.WebApp/Models/Nodes/RequesterOptionViewModel.cs
@@ -8,5 +8,19 @@ namespace BlockBase.Dapps.CloudManager.WebApp.Models.Nodes
     public class RequesterOptionViewModel
     {
         public string Title { get; set; }
+
+        public ConfigNode Full { get; set; }
+        public ConfigNode Validator { get; set; }
+        public ConfigNode History { get; set; }
+        public double MinimumCandidatureStake { get; set; }
+        public int TimeBetweenBlocks { get; set; }
+
+        public int BlockSize { get; set; }
+
+    }
+    public class ConfigNode{
+        public double MinPaymentPerBlock { get; set; }
+        public double MaxPaymentPerBlock { get; set; }
+        public int Required { get; set; }
     }
 }
